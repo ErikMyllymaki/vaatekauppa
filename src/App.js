e013e268b3f8b9db3e667d1b712a14887a932ef2
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-03-18 11:41:04 +0200

    muokattu app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <>
       <Navbar/>
+      <div>
+        <p>JOTAIN TEKSTIÄ</p>
+      </div>
+      <div>
+        <p>tuotekategoriakuvat</p>
+      </div>
+      <Footer/>
     </>
   );
 }
