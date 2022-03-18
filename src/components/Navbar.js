9969176021d8d92607319a5e269b544177073079
ErikMyllymaki <erik.myl01@gmail.com>
2022-03-18 11:42:14 +0200

    turhien juttujen poistoa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from 'react'
+// import './App.css';
+
+
+
+
 
 export default function Navbar() {
+
+    
   return (
-    <div>Navbar</div>
+
+    <div className='container-fluid juttu'>
+        <div className="row juttu">
+            <div className='text-center juttu' >
+                blabla
+            </div>
+        </div>
+    </div>
   )
 }
