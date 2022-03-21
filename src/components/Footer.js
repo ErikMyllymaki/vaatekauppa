20738ce7402b9f18afc42053a41247dbc559b408
ErikMyllymaki <erik.myl01@gmail.com>
2022-03-21 21:23:41 +0200

    footerin fixaus

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default function Footer() {
   return (
+    <div className="container-fluid position-relative">
     <footer className="footer">
-      <div className="container">
+      <div>
         <span>© Vaatekauppa 2022</span>
       </div>
     </footer>
+    </div>
   );
 }
