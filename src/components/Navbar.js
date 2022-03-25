8d98f596a6431790c77e68831b651659a37745ea
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-03-25 12:43:09 +0200

    korjattu kirjotusvirhe

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ export default function Navbar({url}) {
     <>
       <nav className="navbar navbar-expand-lg navbar-light ">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="#">Vaatekauppa</Link>
+          <Link className="navbar-brand" to="/">Vaatekauppa</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
