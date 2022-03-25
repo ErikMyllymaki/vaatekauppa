f2a0f1ae1bfd22ddbb0486771ba8743270bedded
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-03-25 12:35:33 +0200

    korjattu kirjotusvirhe

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom';
 
-export default function Navbar(url) {
+export default function Navbar({url}) {
   const [categories,setCategories] = useState([]);
 
   useEffect(() => {
