9f142b0de714a5359c615e09af7ffd9a943b670e
Jennivis <n1vije00@students.oamk.fi>
2022-03-28 13:15:33 +0300

    Lisätty kuviin hover-toiminnot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,17 +9,29 @@ export default function Home() {
         <p className='welcome'>Tervetuloa vaatekauppaan!</p>
       </div>
       <div className="col-8">
-        <div className='ylaosat'>
-          <Link to="/products/1"><img src={require('../images/ylaosatmuokattu.jpg')} /></Link>
+        <div className='container_img'>
+          <Link to="/products/1"><img src={require('../images/ylaosatmuokattu.jpg')} className='image' /></Link>
+          <div className='middle'>
+            <div className='text'>Yläosat</div>
+          </div>
         </div>
-        <div className='alaosat'>
-        <Link to="/products/2"><img src={require('../images/alaosatmuokattu.jpg')} /></Link>
+        <div className='container_img'>
+          <Link to="/products/2"><img src={require('../images/alaosatmuokattu.jpg')} className='image' /></Link>
+          <div className='middle'>
+            <div className='text'>Alaosat</div>
+          </div>
         </div>
-        <div className='kengat'>
-         <Link to="/products/3"><img src={require('../images/kengatmuokattu.jpg')} /></Link>
+        <div className='container_img'>
+          <Link to="/products/3"><img src={require('../images/kengatmuokattu.jpg')} className='image' /></Link>
+          <div className='middle'>
+            <div className='text'>Kengät</div>
+          </div>
         </div>
-        <div className='asusteet'>
-        <Link to="/products/4"><img src={require('../images/asusteetmuokattu.jpg')} /></Link>
+        <div className='container_img'>
+          <Link to="/products/4"><img src={require('../images/asusteetmuokattu.jpg')} className='image' /></Link>
+          <div className='middle'>
+            <div className='text'>Asusteet</div>
+          </div>
         </div>
       </div>
     </div>
