f646b9a6f7142b06ad61b69515fa110bd1549be5
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-03-31 12:26:01 +0300

    lisätty changeAmount ja tilamuuttujia

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import uuid from 'react-uuid';
 import App from '../App';
-import removeFromCart from '../App'
+import removeFromCart from '../App';
+import { useState } from 'react';
+import updateAmountfrom from '../App';
 
-export default function Order({cart}) {
+export default function Order({cart,removeFromCart,updateAmount}) {
+    const [inputs,_] = useState([]);
+    const [inputIndex, setInputIndex] = useState(-1);
+
     let sum = 0;
+
+    function changeAmount(e,product) {
+        updateAmount(e.target.value,product);
+    }
+
   return (
     <div>
         <h3 className=''>Items in cart</h3>
         <table className='table'>
         <tbody>
-            {cart.map(product => {
+            {cart.map((product,index) => {
                 sum+=parseFloat(product.price);
                 return (
                     <tr key={uuid()}>
                         <td>{product.name}</td>
                         <td>{product.price} €</td>
                         <td>
-                            <input style={{width: '60px'}} value={product.amount} onChange={e => changeAmount(e,product)} />
+                            <input ref={inputs[index]} style={{width: '60px'}} value={product.amount} onChange={e => changeAmount(e,product)} />
                         </td>
                         <td><a href='#' onClick={() => removeFromCart(product)}>Delete</a></td>
                     </tr>
