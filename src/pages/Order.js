98f9993ff02ed3ee0dc6e8f8fe73053e7c92bde6
ErikMyllymaki <erik.myl01@gmail.com>
2022-04-03 20:34:06 +0300

    showTotalPrice functio lisätty

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -7,57 +7,68 @@ import updateAmount from '../App';
 import { createRef } from 'react';
 
 
-export default function Order({cart,removeFromCart,updateAmount}) {
+export default function Order({ cart, removeFromCart, updateAmount }) {
 
-    
-    const [inputs,_] = useState([]);
-    const [inputIndex, setInputIndex] = useState(-1);
 
-    let sum = 0;
+  const [inputs, _] = useState([]);
+  const [inputIndex, setInputIndex] = useState(-1);
 
-    useEffect(() => {
-        for (let i = 0;i<cart.length;i++) {
-          inputs[i] = createRef();
-        }
-      }, [cart.length])
-    
-    useEffect(() => {
-        if (inputs.length > 0 && inputIndex > -1 && inputs[inputIndex].current !== null) {
-          inputs[inputIndex].current.focus();
-        }
-      }, [cart])
+  let sum = 0;
 
-    function changeAmount(e,product,index) {
-        updateAmount(e.target.value,product);
-        setInputIndex(index);
+  useEffect(() => {
+    for (let i = 0; i < cart.length; i++) {
+      inputs[i] = createRef();
     }
+  }, [cart.length])
+
+  useEffect(() => {
+    if (inputs.length > 0 && inputIndex > -1 && inputs[inputIndex].current !== null) {
+      inputs[inputIndex].current.focus();
+    }
+  }, [cart])
+
+  function changeAmount(e, product, index) {
+    updateAmount(e.target.value, product);
+    setInputIndex(index);
+  }
+
+  function showTotalPrice(sum) {
+    if (sum > 0) {
+      return (
+      <tr key={uuid()}>
+        <td></td>
+        <td>Yhteishinta: {sum.toFixed(2)} €</td>
+        <td></td>
+        <td></td>
+      </tr>
+    )} else {
+      return <p>Ostoskorissa ei ole tuotteita.</p>
+    }
+    
+ 
+  }
 
   return (
     <div>
-        <h3 className=''>Items in cart</h3>
-        <table className='table'>
+      <h3 className=''>Items in cart</h3>
+      <table className='table'>
         <tbody>
-            {cart.map((product,index) => {
-                sum+=parseFloat(product.price);
-                return (
-                    <tr key={uuid()}>
-                        <td>{product.name}</td>
-                        <td>{product.price} €</td>
-                        <td>
-                            <input type="number" ref={inputs[index]} style={{width: '60px'}} value={product.amount} onChange={e => changeAmount(e,product)} />
-                        </td>
-                        <td><a href='#' onClick={() => removeFromCart(product)}>Delete</a></td>
-                    </tr>
-                )
-            })}
-            <tr key={uuid()}>
-                <td></td>
-                <td>Yhteishinta: {sum.toFixed(2)} €</td>
-                <td></td>
-                <td></td>
-            </tr>
+          {cart.map((product, index) => {
+            sum += parseFloat(product.price);
+            return (
+              <tr key={uuid()}>
+                <td>{product.name}</td>
+                <td>{product.price} €</td>
+                <td>
+                  <input type="number" ref={inputs[index]} style={{ width: '60px' }} value={product.amount} onChange={e => changeAmount(e, product)} />
+                </td>
+                <td><a href='#' onClick={() => removeFromCart(product)}>Delete</a></td>
+              </tr>
+            )
+          })}
+          {showTotalPrice(sum)}
         </tbody>
-        </table>
+      </table>
     </div>
   )
 }
