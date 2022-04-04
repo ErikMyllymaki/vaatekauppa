dd92ed3307e944d48eadbecab34580d990e0b53e
Jenni Lohi <jennilohi@hotmail.com>
2022-04-04 13:13:40 +0300

    Etusivun kuvia säädetty ja linkit lisätty teksteihin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,29 +8,29 @@ export default function Home() {
       <div className="col d-flex justify-content-center">
         <p className='welcome'>Tervetuloa vaatekauppaan!</p>
       </div>
-      <div className="col-8">
+      <div className="col-6">
         <div className='container_img'>
           <Link to="/products/1"><img src={require('../images/ylaosatmuokattu.jpg')} className='image' /></Link>
           <div className='middle'>
-            <div className='text'>Yläosat</div>
+            <a className='text' href="/products/1">Yläosat</a>
           </div>
         </div>
         <div className='container_img'>
           <Link to="/products/2"><img src={require('../images/alaosatmuokattu.jpg')} className='image' /></Link>
           <div className='middle'>
-            <div className='text'>Alaosat</div>
+            <a className='text' href="/products/2">Alaosat</a>
           </div>
         </div>
         <div className='container_img'>
           <Link to="/products/3"><img src={require('../images/kengatmuokattu.jpg')} className='image' /></Link>
           <div className='middle'>
-            <div className='text'>Kengät</div>
+            <a className='text' href="/products/3">Kengät</a>
           </div>
         </div>
         <div className='container_img'>
           <Link to="/products/4"><img src={require('../images/asusteetmuokattu.jpg')} className='image' /></Link>
           <div className='middle'>
-            <div className='text'>Asusteet</div>
+            <a className='text' href="/products/4">Asusteet</a>
           </div>
         </div>
       </div>
