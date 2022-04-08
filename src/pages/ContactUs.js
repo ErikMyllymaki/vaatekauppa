749bc3673a167ccd74dcc5d4b5e707a70bf8651d
jonirenko <93704954+jonirenko@users.noreply.github.com>
2022-04-08 14:41:27 +0300

    Formin säätöä

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 export default function ContactUs() {
-
+    
     function sendMessage(e) {
         e.preventDefault();
         alert("Kiitos viestistäsi!");  
@@ -12,12 +12,12 @@ export default function ContactUs() {
             <div>
                 <h2>Ota yhteyttä</h2>
                 <form onSubmit={sendMessage}>
-                    <label htmlFor="name">Nimi:</label><br/>
-                    <input type="text" name="name" id="name" required/><br/>
-                    <label htmlFor="name">Sähköposti:</label><br/>
-                    <input type="email" name="email" id="email" required/><br/>
-                    <label htmlFor="message">Viesti:</label><br/>
-                    <textarea name="message" rows="8" cols="30" required/><br/>
+                    <label for="name">Nimi:</label><br/>
+                    <input type="text" name="name" id="name" placeholder="Nimi" required/><br/>
+                    <label for="name">Sähköposti:</label><br/>
+                    <input type="email" name="email" id="email" placeholder="esimerkki@esimerkki.com" required/><br/>
+                    <label for="message">Viesti:</label><br/>
+                    <textarea name="message" rows="8" cols="30" placeholder="Viestisi tähän" required/><br/>
                     <button type="submit">Lähetä</button>
                 </form>
             </div>
