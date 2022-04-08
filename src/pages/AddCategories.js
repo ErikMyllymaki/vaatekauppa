d33e585ba8f05dded0db1374f7e3e5b0bbd26054
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-04-08 10:30:37 +0300

    lisätty form

--- src/pages/AddCategories.js
+++ src/pages/AddCategories.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function AddCategories() {
   return (
-    <div>AddCategories</div>
+    <>
+      <form>
+        <h4>Lisää kategoria</h4>
+        <label for="categoryName">Kategorianimi</label>
+        <input type="text" name="categoryName" id="categoryName"></input>
+        <input type="submit" class="btn btn-primary" value="Lisää kategoria"></input>
+      </form>
+    </>
   )
 }
