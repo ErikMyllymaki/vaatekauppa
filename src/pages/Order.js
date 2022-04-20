e6919d77c6b24bec054bf4a7b912a87852d94550
jonirenko <93704954+jonirenko@users.noreply.github.com>
2022-04-20 12:18:11 +0300

    Lisätty tyhjennä ostoskori-nappi

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -115,6 +115,7 @@ export default function Order({ cart, removeFromCart, updateAmount, url, empty }
       </table>
       {cart.length > 0 &&
       <>
+      <div><button onClick={() => empty()}>Tyhjennä ostoskori</button></div>
       <button onClick={() => setActive(!active)}>{active ? 'Piilota tilauslomake' : 'Tilaa'}</button>
       <form onSubmit={order} className={active ? 'shown' : 'hidden'}>
       <h3>Asiakastiedot</h3>
